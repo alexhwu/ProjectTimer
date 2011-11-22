fdaf58fad8edaacd69676987a9f2487526b1a76a
Alex Wu <alex.h.wu@gmail.com>
2011-11-21 21:49:46 -0800

    Support multiple timers, start/paus/resume.

--- src/com/thealexwu/TaskTimerActivity.java
+++ src/com/thealexwu/TaskTimerActivity.java
@@ -1,9 +1,12 @@
 package com.thealexwu;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.text.Layout;
+import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
@@ -12,12 +15,13 @@ import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class TaskTimerActivity extends Activity {
-    ToggleButton startStopBtn;
-    TextView timeText;
-    TextView taskLabel;
+    //ToggleButton startStopBtn;
+    //TextView timeText;
+    //TextView taskLabel;
     
     LayoutParams startStopBtnLayoutParams;
     LayoutParams mtlParams;
@@ -31,16 +35,17 @@ public class TaskTimerActivity extends Activity {
     TableRow tr;
     LinearLayout ll;
    
-    
+    Timer timer = new Timer();
+    final Handler handler = new Handler();
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        
         ll = (LinearLayout) findViewById(R.id.linearLayout);
         Button btn = (Button) findViewById(R.id.newTimerBtn);
-        
        
         startStopBtnLayoutParams = ((Button) findViewById(R.id.button1)).getLayoutParams();
         timeTextParams = ((TextView) findViewById(R.id.timeText)).getLayoutParams();
@@ -53,13 +58,11 @@ public class TaskTimerActivity extends Activity {
         btn.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View v) {
-			
+				
 				 innerTl = new TableLayout(v.getContext());
 				 innerTl.setLayoutParams(itlParams);
-				 //innerTl.setBackgroundColor(Color.YELLOW);
 				 
-				 
-				 timeText = new TextView(v.getContext());
+				 final TextView timeText = new TextView(v.getContext());
 				 timeText.setText("00:00:00");
 				 timeText.setLayoutParams(timeTextParams);
 				 timeText.setTextSize(28);
@@ -68,7 +71,7 @@ public class TaskTimerActivity extends Activity {
 				 _tr2.addView(timeText);
 				 innerTl.addView(_tr2);
 				 
-				 taskLabel = new TextView(innerTl.getContext());
+				 TextView taskLabel = new TextView(innerTl.getContext());
 				 taskLabel.setText("New task here");
 				 taskLabel.setLayoutParams(taskLabelParams);
 				 taskLabel.setTextSize(18);
@@ -77,7 +80,7 @@ public class TaskTimerActivity extends Activity {
 				 _tr3.addView(taskLabel);
 				 innerTl.addView(_tr3);
 				 
-				// add horizontal line
+				 // add horizontal line
 				 ViewGroup.LayoutParams p = new ViewGroup.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, 1);
 				 
 				 View hrView = new View(v.getContext());
@@ -87,30 +90,70 @@ public class TaskTimerActivity extends Activity {
 				
 				 innerTl.addView(hrView);
 				 
-				 
-				 
-				 
-				 
 				 mainTl = new TableLayout(v.getContext());
 				 mainTl.setLayoutParams(mtlParams);
 				 
-				 
-				 //mainTl.setBackgroundColor(Color.BLUE);
 				 ViewGroup.LayoutParams btnParams = new ViewGroup.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, 55);
 				 final float scale = getResources().getDisplayMetrics().density;
 				 int pixels = (int) (65 * scale + 0.5f);
 				    
-				 startStopBtn = new ToggleButton(v.getContext()); 
+				 final ToggleButton startStopBtn = new ToggleButton(v.getContext()); 
 				 startStopBtn.setText("Start");
 				 startStopBtn.setTextSize(12);
-				 //startStopBtn.setLayoutParams(btnParams);
 				 startStopBtn.setHeight(pixels);
 				 startStopBtn.setWidth(pixels);
-				 //startStopBtn.setBackgroundColor(Color.BLUE);
-				 //startStopBtn.setLayoutParams(startStopBtnLayoutParams);
+				 
+				 
+				 
+				 
+				 startStopBtn.setOnClickListener(new View.OnClickListener() {
+					
+					 
+					 TimerTask timerTask = null;
+					 int counter = 0;
+					public void onClick(View v) {
+						
+						
+						
+						if (startStopBtn.isChecked()) {
+							startStopBtn.setText("Stop");
+							timerTask = new TimerTask() {
+								
+								public void run() {
+									handler.post(new Runnable() {
+										
+										
+										public void run() {
+											counter++; // seconds
+											int hour = (int) counter / 6000;
+											int min = (int) counter / 60;
+											int sec = counter % 60;
+											
+											
+											String counterStr = (hour < 10 ? "0"+hour : hour) + ":" + (min < 10 ? "0"+min : min) + ":" + (sec < 10 ? "0"+sec : sec);
+											timeText.setText(counterStr);
+											//Toast toast = Toast.makeText(TaskTimerActivity.this, counterStr, Toast.LENGTH_SHORT);
+											//toast.show();
+										}
+									});
+								}
+							};
+							
+							timer.schedule(timerTask, 0, 1000);
+							
+						
+						} else {
+							startStopBtn.setText("Start");
+							handler.removeCallbacks(timerTask);
+							
+							if (timerTask != null) {
+								timerTask.cancel();
+							}
+						}
+					}
+				 });
 				 
 				 TableRow _tr = new TableRow(mainTl.getContext());
-				 //_tr.setBackgroundColor(Color.GREEN);
 				 _tr.addView(startStopBtn);
 				 _tr.addView(innerTl);
 				 
@@ -120,21 +163,10 @@ public class TaskTimerActivity extends Activity {
 				 _tr.setLayoutParams(tableRowParams);
 				 
 				 
-				 
-				 
-				 
-				 
-				 
-				 
 				 mainTl.addView(_tr);
 				
 				 // add timer and task label to inner table
 				 ll.addView(mainTl);
-				 
-				 
-				 
-				 
-				 
 					 
 				 
 			}
@@ -142,5 +174,7 @@ public class TaskTimerActivity extends Activity {
 		
 		
     }
+    
+    
     
 }
